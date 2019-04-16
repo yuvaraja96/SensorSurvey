5a1133d102c333bfa1712f68780b138173b68c45
Yuvaraja <yuvarajaj-wa14@student.tarc.edu.my>
2019-04-16 12:22:35 +0800

    SensorSurvey Practical

---
+++ app/src/main/java/com/example/android/sensorsurvey/MainActivity.java
@@ -0,0 +1,42 @@
+
+package com.example.android.sensorsurvey;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mSensorManager =
+                (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList  =
+                mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+
+        StringBuilder sensorText = new StringBuilder();
+
+        for (Sensor currentSensor : sensorList ) {
+            sensorText.append(currentSensor.getName()).append(
+                    System.getProperty("line.separator"));
+        }
+
+
+        TextView sensorTextView = (TextView) findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
+    }
+}
